541279d33a037acd4bccac9d289467a4a297ab0d
banyudu <banyudu@gmail.com>
2023-10-22 10:32:57 +0800

    fix: nanoid index.browser

--- src/useCachedService.ts
+++ src/useCachedService.ts
@@ -2,9 +2,9 @@
  * 类似于 useService，但是默认使用缓存，并提供 refresh 方法
  */
 
-import { useState } from 'react'
 import useService, { HookResult } from './useService'
 import { random } from './utils'
+import { atom, useAtom } from 'jotai'
 
 export interface CachedHookResult<Result = any> extends HookResult<Result> {
   refresh: () => void
@@ -17,8 +17,11 @@ const useCachedService = <Result = any, Params = any> (
   skip?: (p: Params) => boolean
 ): CachedService<Result, Params> => {
   const innerHook = useService(fetcher, skip)
+  // const refreshKeyAtom = useMemo(() => atom<string>(random()), [])
+  const refreshKeyAtom = atom<string>(random())
   return (params?: Params): CachedHookResult => {
-    const [refreshKey, setRefreshKey] = useState(random())
+    // const [refreshKey, setRefreshKey] = useState(random())
+    const [refreshKey, setRefreshKey] = useAtom(refreshKeyAtom)
 
     const result = innerHook(params, refreshKey)
     return {
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,7 @@
-import { nanoid } from 'nanoid/index.browser'
 export const random = (): string => {
   try {
+    // eslint-disable-next-line
+    const { nanoid } = require('nanoid')
     return nanoid()
   } catch (error) {
     return String(Math.random()).substring(2, 14)
