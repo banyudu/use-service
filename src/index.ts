fdba4ee59c2f9596cfdcb70fb0e907c63bf20d3e
banyudu <banyudu@gmail.com>
2024-05-02 16:28:42 +0800

    feat: export types from swr

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import useService, { type HookResult } from './useService'
 import useCachedService, { type CachedHookResult } from './useCachedService'
+export { SWRConfig, SWRConfiguration, SWRResponse, SWRHook } from 'swr'
 
 export {
   useService,
